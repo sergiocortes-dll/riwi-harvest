793901d701108856b3e1ac5186b7be699bd1888d
miguelanga1604@gmail.com <miguelanga1604@gmail.com>
2025-08-24 13:19:34 -0500

    feat: new collumn

--- apps/backend/database/riwi_harvest_db.sql
+++ apps/backend/database/riwi_harvest_db.sql
@@ -3,6 +3,7 @@ USE riwi_harvest_db;
 
 CREATE TABLE `coders` (
 	`id_coder` INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+	`full_name` VARCHAR(100) NOT NULL,
 	`name` VARCHAR(50) NOT NULL,
 	`lastname` VARCHAR(50) NOT NULL,
 	`email` VARCHAR(100) NOT NULL UNIQUE,
